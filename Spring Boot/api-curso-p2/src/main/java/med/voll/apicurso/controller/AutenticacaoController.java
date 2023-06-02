566be310458cfa7dfa877ece26eb4b86a5cabae7
Wesley Cruz <wesley.leoncio.izi@gmail.com>
2023-06-02 12:37:54 -0300

    Configurações do json web token adicionadas

--- Spring Boot/api-curso-p2/src/main/java/med/voll/apicurso/controller/AutenticacaoController.java
+++ Spring Boot/api-curso-p2/src/main/java/med/voll/apicurso/controller/AutenticacaoController.java
@@ -1,7 +1,10 @@
 package med.voll.apicurso.controller;
 
 import jakarta.validation.Valid;
+import med.voll.apicurso.model.usuario.entity.Usuario;
 import med.voll.apicurso.model.usuario.request.UserRequest;
+import med.voll.apicurso.model.usuario.response.TokenJwtResponse;
+import med.voll.apicurso.service.TokenService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -13,16 +16,22 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/usuario")
-public class UsuarioController {
+@RequestMapping("/login")
+public class AutenticacaoController {
 
     @Autowired
     private AuthenticationManager manager;
-    @PostMapping("/login")
-    public ResponseEntity efetuarLogin(@RequestBody @Valid UserRequest userLogin) {
+
+    @Autowired
+    private TokenService tokenService;
+
+    @PostMapping()
+    public ResponseEntity<TokenJwtResponse> efetuarLogin(@RequestBody @Valid UserRequest userLogin) {
         UsernamePasswordAuthenticationToken token = new UsernamePasswordAuthenticationToken(userLogin.login(), userLogin.senha());
         Authentication authentication = manager.authenticate(token);
-        return ResponseEntity.ok().build();
+        String tokenJWT = tokenService.gerarToken((Usuario) authentication.getPrincipal());
+
+        return ResponseEntity.ok(new TokenJwtResponse(tokenJWT));
     }
 
 }
